957a1d66c77c236c8ed0e8eb292b8f63cfa1430e
Sebastian <dersebastianposch@gmail.com>
2019-01-03 23:12:53 +0100

    Added Boston Marathon & Boxing Day

--- js/countdowns.js
+++ js/countdowns.js
@@ -116,3 +116,49 @@ function Countdown_AshWednesday() {
     }
   }, 1000);
 }
+// Boston Marathon
+function Countdown_BostonMarathon() {
+  var countDownDate = new Date("Apr 16, 2019 00:00:00").getTime();
+  var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("DAYS_BOSTONMARATHON").innerHTML = days + "";
+    document.getElementById("HOURS_BOSTONMARATHON").innerHTML = hours + "";
+    document.getElementById("MINUTES_BOSTONMARATHON").innerHTML = minutes + "";
+    document.getElementById("SECONDS_BOSTONMARATHON").innerHTML = seconds + "";
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("DAYS_BOSTONMARATHON").innerHTML = "X";
+      document.getElementById("HOURS_BOSTONMARATHON").innerHTML = "X";
+      document.getElementById("MINUTES_BOSTONMARATHON").innerHTML = "X";
+      document.getElementById("SECONDS_BOSTONMARATHON").innerHTML = "X";
+    }
+  }, 1000);
+}
+// Boxing Day
+function Countdown_BoxingDay() {
+  var countDownDate = new Date("Dec 26, 2019 00:00:00").getTime();
+  var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("DAYS_BOXINGDAY").innerHTML = days + "";
+    document.getElementById("HOURS_BOXINGDAY").innerHTML = hours + "";
+    document.getElementById("MINUTES_BOXINGDAY").innerHTML = minutes + "";
+    document.getElementById("SECONDS_BOXINGDAY").innerHTML = seconds + "";
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("DAYS_BOXINGDAY").innerHTML = "X";
+      document.getElementById("HOURS_BOXINGDAY").innerHTML = "X";
+      document.getElementById("MINUTES_BOXINGDAY").innerHTML = "X";
+      document.getElementById("SECONDS_BOXINGDAY").innerHTML = "X";
+    }
+  }, 1000);
+}
